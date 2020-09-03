28a4c0e78545fd9763e0552ca53042fb23a5b864
hiroyukimizukami <majestaaa@gmail.com>
2020-09-04 01:58:46 +0900

    added fb

---
+++ FrameworkB/FrameworkB/FrameworkB.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkB.h
+//  FrameworkB
+//
+//  Created by Hiroyuki Mizukami on 2020/09/04.
+//  Copyright © 2020 Hiroyuki Mizukami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkB.
+FOUNDATION_EXPORT double FrameworkBVersionNumber;
+
+//! Project version string for FrameworkB.
+FOUNDATION_EXPORT const unsigned char FrameworkBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkB/PublicHeader.h>
+
+
---
+++ FrameworkB/FrameworkBTests/FrameworkBTests.swift
@@ -0,0 +1,34 @@
+//
+//  FrameworkBTests.swift
+//  FrameworkBTests
+//
+//  Created by Hiroyuki Mizukami on 2020/09/04.
+//  Copyright © 2020 Hiroyuki Mizukami. All rights reserved.
+//
+
+import XCTest
+@testable import FrameworkB
+
+class FrameworkBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
