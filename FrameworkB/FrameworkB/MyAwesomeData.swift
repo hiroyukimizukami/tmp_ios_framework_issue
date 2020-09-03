c4616920caa8f8572ceac8b415c82e3d50e95e5b
hiroyukimizukami <majestaaa@gmail.com>
2020-09-04 02:02:00 +0900

    test

---
+++ FrameworkB/FrameworkB/MyAwesomeData.swift
@@ -0,0 +1,14 @@
+//
+//  MyAwesomeData.swift
+//  FrameworkB
+//
+//  Created by Hiroyuki Mizukami on 2020/09/04.
+//  Copyright © 2020 Hiroyuki Mizukami. All rights reserved.
+//
+
+import Foundation
+import FrameworkA
+
+final class MyAwesomeData {
+    
+}
